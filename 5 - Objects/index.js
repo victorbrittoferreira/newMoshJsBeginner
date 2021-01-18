a996f0a8532615a907c60ac10ef73f31d95d50f8
victorbrittoferreira <victorbrittoferreira@gmail.com>
2021-01-18 17:36:33 -0300

    .

---
+++ 5 - Objects/index.js
@@ -0,0 +1,89 @@
+{
+  //48. 1- Basics
+  //Objects = collections of key value pairs
+  //  - Purpose = of an object is:
+  //      1 - group related variables
+  //      2 -functions that should operate on these variables.
+
+  // declared variables --- become properties, when inside and object
+  //let radius = 1;
+  //let x = 1;
+  //let y = 1 ;
+
+  //Method = function is part of an object
+  // the function should be add inside an object
+  //function draw() {} >> draw: function() {}
+  //function move() {} >> move: function() {}
+
+  const circle = {
+    radius: 1,
+    location: {
+      x: 1,
+      y: 1,
+    },
+    isVisible: true,
+    //function name (params)
+    draw: function () {
+        console.log("Draw function");
+    },
+  };
+ 
+  circle.draw(); // method
+}
+{
+  //49. 2- Factory Functions
+}
+{
+  //50. 3- Constructor Functions
+}
+{
+  //51. 4- Dynamic Nature of Objects
+}
+{
+  //52. 5- Constructor Property
+}
+{
+  //53. 6- Functions are Objects
+}
+{
+  //54. 7- Value vs. Reference Types
+}
+{
+  //55. 8- Enumerating Properties of an Object
+}
+{
+  //56. 9- Cloning an Object
+}
+{
+  //57. 10- Garbage Collection
+}
+{
+  //58. 11- Math
+}
+{
+  //59. 12- String
+}
+{
+  //60. 13- Template Literals
+}
+{
+  //61. 14- Date
+}
+{
+  //62. 15- Exercise 1- Address Object
+}
+{
+  //63. 16- Exercise- Factory and Constructor Function
+}
+{
+  //64. 17- Exercise 3- Object Equality
+}
+{
+  //65. 18- Exercise 4- Blog Post Object
+}
+{
+  //66. 19- Exercise 5- Constructor Functions
+}
+{
+  //67. 20- Exercise 6- Price Range Objects
+}
